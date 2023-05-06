b074e557e2c5d757afd82bbd7126e5b4eb09d913
kara <ssctonioloriccardo@gmail.com>
2023-05-06 14:49:30 +0200

    close #4
    Aggiunta interfacce da sviluppare sia per main che per test.

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,8 @@
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+        // TODO
+        return new String("");
+    }
+}
---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,12 @@
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        // TODO
+        return null;
+    }
+}
