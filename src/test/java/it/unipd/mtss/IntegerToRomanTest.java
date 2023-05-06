0ced2e6a7b4b1fd35290e483e1a261f62208023d
Carraro-Riccardo <riccardocarraro69@gmail.com>
2023-05-06 17:13:35 +0200

    close #6
    Test per numeri negativi

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,10 +1,20 @@
 package it.unipd.mtss;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class IntegerToRomanTest {
-    @Test
-    public void testConvert() {
+    static IntegerToRoman converter = null;
 
+    @BeforeClass
+    public static void initializeConverter() {
+        converter = new IntegerToRoman();
     }
+
+    @Test(expected = NumberUnderZeroException.class)
+    public void testNumberUnderZero() throws NumberUnderZeroException, NumberAEFiveThousand {
+        int arabic_number = -5;
+        converter.convert(arabic_number);
+    }
+
 }
