e9897d7e9cc0451527799008c693587dd6ae89f5
Carraro-Riccardo <riccardocarraro69@gmail.com>
2023-05-06 17:21:21 +0200

    close #9
    Test per ascii art I

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,10 +1,26 @@
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RomanPrinterTest {
-    @Test
-    public void testPrint() {
+    static RomanPrinter printer;
 
+    @BeforeClass
+    public static void initializePrinter() {
+        printer = new RomanPrinter();
+    }
+
+    @Test
+    public void testPrintI() throws NumberUnderZeroException, NumberAEFiveThousand {
+        int arabic_number = 1;
+        String ascii_art_for_I = printer.print(arabic_number);
+        assertEquals(ascii_art_for_I, new String(" _____ \n" +
+                "|_   _|\n" +
+                "  | |  \n" +
+                "  | |  \n" +
+                " _| |_ \n" +
+                "|_____|\n"));
     }
 }
